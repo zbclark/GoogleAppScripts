172432d1dd0ad463f7cd6e65cc2a656cc5ce84ef
Zac Clark <95437293+zbclark@users.noreply.github.com>
2026-04-23 14:39:51 -0500

    Delete apps-scripts/modelOptimizer directory

--- apps-scripts/modelOptimizer/utilities/buildRecentYears.js
+++
@@ -1,5 +0,0 @@
-// Utility to build an array of recent years
-module.exports = function buildRecentYears(season, yearCount = 5) {
-  const baseYear = season || new Date().getFullYear();
-  return Array.from({ length: yearCount }, (_, idx) => baseYear - idx);
-};
